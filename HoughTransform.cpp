9014d4ea1f7358aa4d72b986d67b3fb6cef03cd7
Pavel <p.guigin@gmail.com>
2016-02-13 12:44:43 +0300

    Hough transformation class

---
+++ HoughTransform.cpp
@@ -0,0 +1,114 @@
+#include <cassert>
+
+#include <QtMath>
+#include "HoughTransform.h"
+
+HoughTransform::HoughTransform(double arg_of_max_mod, double value_of_max_mod, double radius_step)
+{
+    Init(arg_of_max_mod, value_of_max_mod, radius_step);
+}
+
+HoughTransform::~HoughTransform()
+{
+    DeleteTable();
+}
+
+void HoughTransform::Init(double arg_of_max_abs, double value_of_max_abs, double radius_step)
+{
+    double arg_sqr = arg_of_max_abs * arg_of_max_abs;
+    double value_sqr = value_of_max_abs * value_of_max_abs;
+    radius_step = qMax(_MIN_RADIUS_STEP, qMin(radius_step, _MAX_RADIUS_STEP));
+
+    this->_radius_step = radius_step;
+    _max_radius = qCeil(qSqrt(arg_sqr + value_sqr));
+    //Один столбец для нуля
+    _columns_as_radius_values = qRound(_max_radius / radius_step) + 1;
+
+    CreateTable();
+    Clear();
+}
+
+void HoughTransform::AddPoint(double x, double y)
+{
+    assert(_matrix != nullptr);
+    for (int angle_in_degr = 0; angle_in_degr < _ROWS_AS_ANGLE_VALUES; angle_in_degr++) {
+        double angle_in_rad = qDegreesToRadians(double(angle_in_degr));
+        double radius = x*qCos(angle_in_rad) + y*qSin(angle_in_rad);
+        if (0 <= radius && radius <= _max_radius) {
+            int row = angle_in_degr;
+            int col = qRound(radius / _radius_step);
+            _matrix[row][col]++;
+        }
+    }
+    _is_result_found = false;
+}
+
+double HoughTransform::GetNormalAngleInDegr()
+{
+    if (_is_result_found == false) FindResult();
+    return _res_angle_in_degr;
+}
+
+double HoughTransform::GetNormalRadius()
+{
+    if (_is_result_found == false) FindResult();
+    return _res_radius;
+}
+
+double HoughTransform::GetLineAngleCoef()
+{
+    if (_is_result_found == false) FindResult();
+    // Формула прямой: x*cos(alpha) + y*sin(alpha) = r
+    double angle_in_rad = qDegreesToRadians(_res_angle_in_degr);
+    double angle_coef = -qCos(angle_in_rad)/qSin(angle_in_rad);
+    return angle_coef;
+}
+
+double HoughTransform::GetLineShift()
+{
+    if (_is_result_found == false) FindResult();
+    // Формула прямой: x*cos(alpha) + y*sin(alpha) = r
+    double angle_in_rad = qDegreesToRadians(_res_angle_in_degr);
+    double line_shift = _res_radius/qSin(angle_in_rad);
+    return line_shift;
+}
+
+void HoughTransform::CreateTable()
+{
+    _matrix = new int*[_ROWS_AS_ANGLE_VALUES];
+    for (int i = 0; i < _ROWS_AS_ANGLE_VALUES; ++i) {
+       _matrix[i] = new int[_columns_as_radius_values];
+    }
+}
+
+void HoughTransform::Clear()
+{
+    for (int row = 0; row < _ROWS_AS_ANGLE_VALUES; ++row) {
+        for (int col = 0; col < _columns_as_radius_values; ++col) {
+            _matrix[row][col] = 0;
+        }
+    }
+}
+
+void HoughTransform::DeleteTable()
+{
+    for (int i = 0; i < _ROWS_AS_ANGLE_VALUES; ++i) {
+       delete []_matrix[i];
+    }
+    delete []_matrix;
+}
+
+void HoughTransform::FindResult()
+{
+    int row_of_max = 0, col_of_max = 0;
+    for (int row = 0; row < _ROWS_AS_ANGLE_VALUES; ++row) {
+        for (int col = 0; col < _columns_as_radius_values; ++col) {
+            if (_matrix[row_of_max][col_of_max] < _matrix[row][col]) {
+                row_of_max = row;
+                col_of_max = col;
+            }
+        }
+    }
+    _res_angle_in_degr = row_of_max; _res_radius = col_of_max * _radius_step;
+    _is_result_found = true;
+}
---
+++ HoughTransform.h
@@ -0,0 +1,44 @@
+#ifndef HOUGHTRANSFORM_H
+#define HOUGHTRANSFORM_H
+
+class HoughTransform
+{
+public:
+    HoughTransform() { }
+    HoughTransform(double arg_of_max_mod, double value_of_max_mod, double radius_step = 0.1);
+    ~HoughTransform();
+
+    void Init(double arg_of_max_abs, double value_of_max_abs, double radius_step = 0.1);
+
+    double GetMaxRadius() const { return _max_radius; }
+    double GetRadiusStep() const { return _radius_step; }
+
+    void AddPoint(double x, double y);
+
+    double GetNormalAngleInDegr();
+    double GetNormalRadius();
+
+    double GetLineAngleCoef();
+    double GetLineShift();
+
+    void Clear();
+
+    //Для тестов
+    int ColumnsCnt() const { return _columns_as_radius_values; }
+
+private:
+    void CreateTable();
+    void DeleteTable();
+
+    void FindResult();
+
+    bool _is_result_found = false;
+    const double _MIN_RADIUS_STEP = 0.1, _MAX_RADIUS_STEP = 1;
+    double _res_angle_in_degr, _res_radius;
+    int **_matrix = nullptr;
+    const int _ROWS_AS_ANGLE_VALUES = 360;
+    int _columns_as_radius_values;
+    double _radius_step, _max_radius;
+};
+
+#endif // HOUGHTRANSFORM_H
