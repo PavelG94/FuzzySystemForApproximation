e582d566b3d2925e26b2ef2558b9734dad60925f
Pavel <p.guigin@gmail.com>
2016-04-05 13:11:40 +0300

    Noise generation for input

--- Main.cpp
+++ Main.cpp
@@ -5,15 +5,50 @@
 #include "CntlBuilder.h"
 #include "MainWindow.h"
 
+#include <random>
+
+void GenInputValues(QVector<double> &x_vals, QVector<double> &y_vals,
+                    UnaryFunc f, double x_min, double x_max, double step, bool with_noise)
+{
+    int points_cnt = (x_max - x_min + step) / step; // [x_min, x_max]
+    x_vals.resize(points_cnt);
+    y_vals.resize(points_cnt);
+
+    std::default_random_engine gen;
+    const double mean = 0, stddev = 0.5;
+    std::normal_distribution<double> normal_distr(mean,stddev);
+    int left_bound = 0, right_bound = 1;
+    std::uniform_int_distribution<int> uniform_distr(left_bound,right_bound);
+    for (int i = 0; i < points_cnt; ++i) {
+        double x = x_min + i*step;
+        double y = f(x);
+        if (f.IsLastResValid() == false) {
+            y = 0;
+        } else if (with_noise == true) {
+            double noise_val = normal_distr(gen);
+            int possib = uniform_distr(gen);
+            y = y + possib*noise_val;
+        }
+        x_vals[i] = x;
+        y_vals[i] = y;
+    }
+}
+
 int RunProg(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    UnaryFunc f([](double x)->double { return qSin(x)/x; });
+    UnaryFunc f([](double x)->double {
+        double y = (x != 0)? qSin(x)/x: 1;
+        return y;
+    });
     const double x_min = -10, x_max = 10, step = 0.1;
 
+    QVector<double> x_vals, y_vals;
+    bool with_noise = false;
+    GenInputValues(x_vals, y_vals, f, x_min, x_max, step, with_noise);
     CntlBuilder builder;
-    builder.SetData(f, x_min, x_max, step);
+    builder.SetData(x_vals, y_vals);
 
     MainWindow win;
     QCPRange x_range(x_min, x_max), y_range(-3,3);
